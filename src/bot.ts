01f304684b5308b34f04b11b6c9dae967adfc97f
Thumster <bryan.thum@hotmail.com>
2024-03-30 14:23:18 +0800

    feat: eslint and prettier

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,10 @@
-import { Bot } from "grammy";
-import dotenv from "dotenv";
-import { CommandName, commandDescriptions } from "./commands";
+import { Bot } from 'grammy';
+import dotenv from 'dotenv';
+import { CommandName, commandDescriptions } from './commands';
 
 dotenv.config();
 
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "";
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '';
 
 // Create an instance of the `Bot` class and pass your bot token to it.
 const bot = new Bot(TELEGRAM_BOT_TOKEN);
@@ -13,7 +13,7 @@ bot.api.setMyCommands(
   Object.entries(commandDescriptions).map(([command, description]) => ({
     command,
     description,
-  }))
+  })),
 );
 
 // You can now register listeners on your bot object `bot`.
@@ -21,11 +21,11 @@ bot.api.setMyCommands(
 
 // Handle the /start command.
 bot.command(CommandName.Start, (ctx) =>
-  ctx.reply("Welcome! Up and runninggg.")
+  ctx.reply('Welcome! Up and runninggg.'),
 );
-bot.command(CommandName.Help, (ctx) => ctx.reply("Help is on the wayyy"));
+bot.command(CommandName.Help, (ctx) => ctx.reply('Help is on the wayyy'));
 // Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.on('message', (ctx) => ctx.reply('Got another message!'));
 
 // Now that you specified how to handle messages, you can start your bot.
 // This will connect to the Telegram servers and wait for messages.
