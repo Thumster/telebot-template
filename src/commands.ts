489518f90fcdd3034f2d4c3c646d998f35034dc9
Thumster <bryan.thum@hotmail.com>
2024-03-30 14:15:45 +0800

    feat: grammy typescript base with dotenv

---
+++ src/commands.ts
@@ -0,0 +1,9 @@
+export enum CommandName {
+  Start = "start",
+  Help = "help",
+}
+
+export const commandDescriptions: Record<CommandName, string> = {
+  [CommandName.Start]: "Eh hungry already ah, start the bot lah",
+  [CommandName.Help]: "Heloo, you need my help is it?",
+};
